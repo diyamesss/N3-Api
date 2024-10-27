103de4e6755f4a8ac3f3c6ab0d0d7c67a90f0085
diyamesss <jamesryan.developer@gmail.com>
2024-10-28 00:30:00 +0800

    Update solution and project name

--- obj/Release/net8.0/N3Api.AssemblyInfo.cs
+++ obj/Release/net8.0/N3Api.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("N3Api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d634a17861a6809456d9728716b7b9f69a08b639")]
 [assembly: System.Reflection.AssemblyProductAttribute("N3Api")]
